9b065be2d43d7c2e08afa1c46f42f8987b39b524
9369702788 <67014896+9369702788@users.noreply.github.com>
2026-07-27 15:51:25 +0300

    Update quran_screen.dart

--- lib/features/quran/quran_screen.dart
+++ lib/features/quran/quran_screen.dart
@@ -1,1 +1,52 @@
 
+import 'package:flutter/material.dart';
+
+class QuranScreen extends StatelessWidget {
+  const QuranScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final surahs = [
+      "الفاتحة",
+      "البقرة",
+      "آل عمران",
+      "النساء",
+      "المائدة",
+      "الأنعام",
+      "الأعراف",
+      "الأنفال",
+      "التوبة",
+      "يونس",
+      "هود",
+      "يوسف",
+      "الرعد",
+      "إبراهيم",
+      "الحجر",
+      "النحل",
+      "الإسراء",
+      "الكهف",
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("القرآن الكريم"),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: surahs.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: CircleAvatar(
+              child: Text("${index + 1}"),
+            ),
+            title: Text(
+              surahs[index],
+              textAlign: TextAlign.right,
+            ),
+            trailing: const Icon(Icons.menu_book),
+          );
+        },
+      ),
+    );
+  }
+}
